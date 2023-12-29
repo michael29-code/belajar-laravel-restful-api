ef1961fcbe5e5adf31b5155b9671b9f491c7b6a0
All14nce <135573070+All14nce@users.noreply.github.com>
2023-12-29 11:41:42 +0700

    finishing model address and contact

--- database/migrations/2023_12_29_042223_create_contacts_table.php
+++ database/migrations/2023_12_29_042223_create_contacts_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string("first_name", 100)->nullable(false);
+            $table->string("last_name", 100)->nullable();
+            $table->string("email", 200)->nullable();
+            $table->string("phone", 20)->nullable();
+            $table->unsignedBigInteger("user_id")->nullable(false);
             $table->timestamps();
+            $table->foreign("user_id")->on("users")->references("id");
         });
     }
 
